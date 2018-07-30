b29a9b22be53482c5b2d69681758101ee41098b3
Tuba Kaya <tuba_kaya@outlook.com>
2018-07-30 22:17:30 +0200

    f2-fix-unit-tests: Change diagnostic to warn only when class name ends with tests

--- common/TestSetupGenerator.CodeAnalysis.UnitTests/CodeAnalyzers/TestClassDetectorTests.cs
+++ common/TestSetupGenerator.CodeAnalysis.UnitTests/CodeAnalyzers/TestClassDetectorTests.cs
@@ -8,9 +8,9 @@ namespace TestSetupGenerator.CodeAnalysis.UnitTests.CodeAnalyzers
         [Theory]
         [InlineData("Tests", true)]
         [InlineData("BlaTests", true)]
-        [InlineData("BlaTest", true)]
+        [InlineData("BlaTest", false)]
         [InlineData("Bla", false)]
-        public void ReturnsTrueIfNameContainsTest(string className, bool expected)
+        public void ReturnsTrueIfNameEndsWithTests(string className, bool expected)
         {
             var actual = TestClassDetector.IsTestClass(className);
             Assert.Equal(expected, actual);
