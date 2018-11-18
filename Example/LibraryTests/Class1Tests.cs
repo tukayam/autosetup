8a158d1002d1c6e230c0ee4b287fa7a21323fa14
Tuba Kaya <tuba_kaya@outlook.com>
2018-11-18 21:03:55 +0100

    🎉 Updates readme.md

---
+++ Example/LibraryTests/Class1Tests.cs
@@ -0,0 +1,16 @@
+using LibraryUnderTest;
+using Xunit;
+
+namespace LibraryTests
+{
+    public class Class1Tests
+    {
+       
+        
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Example/LibraryUnderTest/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LibraryUnderTest
+{
+    public interface IDependency
+    {
+    }
+
+    public class Class1
+    {
+        private readonly IDependency _dependency;
+
+        public Class1(IDependency dependency)
+        {
+            _dependency = dependency;
+        }
+    }
+}
